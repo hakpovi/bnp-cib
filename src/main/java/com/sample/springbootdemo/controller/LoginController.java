f0f3a5cd6c8f06eec3eb38a68550bf3a026615d0
HERVE AKPOVI <akpoviherve@yahoo.fr>
2023-10-24 00:17:50 +0000

    Update file LoginController.java

--- src/main/java/com/sample/springbootdemo/controller/LoginController.java
+++ src/main/java/com/sample/springbootdemo/controller/LoginController.java
@@ -3,7 +3,7 @@ package com.sample.springbootdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class LoginController {
 
