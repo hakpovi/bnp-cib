5a3897621c8f0c0a8d1376386e80eafb8bc2bc8b
HERVE AKPOVI <akpoviherve@yahoo.fr>
2023-10-24 01:18:57 +0000

    Update file LoginController.java

--- src/main/java/com/sample/springbootdemo/controller/LoginController.java
+++ src/main/java/com/sample/springbootdemo/controller/LoginController.java
@@ -1,5 +1,5 @@
 package com.sample.springbootdemo.controller;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
