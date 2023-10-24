774f94f198b0fc81111c0710aa1db5e0d39dceb9
HERVE AKPOVI <akpoviherve@yahoo.fr>
2023-10-24 10:22:52 +0000

    Update file LoginController.java

--- src/main/java/com/sample/springbootdemo/controller/LoginController.java
+++ src/main/java/com/sample/springbootdemo/controller/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 
     
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     @ResponseBody
     public String login(){
         return "hello world";
